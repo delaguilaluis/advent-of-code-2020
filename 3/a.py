0898f50830952868b3e46337accbab1a1e04aea3
Luis Del Águila <luisdelaguila@outlook.com>
2020-12-06 10:12:35 -0600

    Fix solution for 3.a

--- 3/a.py
+++ 3/a.py
@@ -2,12 +2,14 @@ with open('input.txt', "r+") as file:
     contents = file.read()
 
 entries = list(filter(lambda x: x != '', contents.split('\n')))
+spaceCount = len(entries[0])
 tree = '#'
 nextSpace = 0
 treeCount = 0
 
 for entry in entries:
-    if entry[nextSpace % 3] == tree:
+    currentSpace = entry[nextSpace % spaceCount]
+    if currentSpace == tree:
         treeCount += 1
     nextSpace += 3
 
