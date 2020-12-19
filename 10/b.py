01b8ed59299e514d8c6e516b1d0cc5a25b3a797d
Luis Del Águila <luisdelaguila@outlook.com>
2020-12-19 11:45:02 -0600

    Make advancements in solution for puzzle 10.b (WIP)

--- 10/b.py
+++ 10/b.py
@@ -4,33 +4,32 @@ with open('input.txt', "r+") as file:
     contents = file.read()
 
 
-def reduz(numbers, accum):
+def reduz(numbers, accum, skip=True):
+    print(numbers)
     if len(numbers) == 1:
-        print()
+        print(accum + 1, '\n')
         return 1
 
     if len(numbers) >= 4 and numbers[3] - numbers[0] == 3:
-        copy = numbers.copy()
+        print('first scenario')
+        accum += reduz(numbers[2:], accum)
+
+        copy = numbers[1:]
         copy.pop(1)
-        print(copy)
         accum += reduz(copy, accum)
 
-        copyTwo = numbers.copy()
-        copyTwo.pop(2)
-        print(copyTwo)
-        accum += reduz(copyTwo, accum)
-    else:
-        if len(numbers) >= 3 and numbers[2] - numbers[0] <= 3:
-            copy = numbers.copy()
-            copy.pop(1)
-            print(copy)
-            accum += reduz(copy, accum)
+        accum += reduz(numbers[3:], accum)
 
-    print(numbers[1:])
-    return reduz(
-        numbers[1:],
-        accum
-    )
+        print('base scenario without skipping')
+        return reduz(numbers[1:], accum, skip=False)
+
+    else:
+        if skip and len(numbers) >= 3 and numbers[2] - numbers[0] <= 3:
+            print('second scenario')
+            accum += reduz(numbers[2:], accum)
+
+    print('base scenario')
+    return reduz(numbers[1:], accum)
 
 
 entries = list(filter(lambda x: x != '', contents.split('\n')))
